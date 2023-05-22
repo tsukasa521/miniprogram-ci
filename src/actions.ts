8dcdcd028adc1c8ebc95cfd562f204c23f30b45e
kk++ <kkplusplus@163.com>
2023-05-22 18:08:32 +0800

    docs: 添加注释

--- src/actions.ts
+++ src/actions.ts
@@ -2,11 +2,13 @@ import { Logger } from './logger';
 import { getConfigList, updateVersion, publishMiniprogram } from './services';
 import { PublishMiniprogramActionOptions, listConfigActionOptions } from './types';
 
-export const publishMiniprogramProjectAction = async (
-  projectName: string,
-  config: string,
-  options: PublishMiniprogramActionOptions
-) => {
+/**
+ * 发布小程序
+ * @param projectName 小程序项目名 
+ * @param config 配置文件路径
+ * @param options 
+ */
+export async function publishMiniprogramProjectAction(projectName: string, config: string, options: PublishMiniprogramActionOptions) {
   try {
     Logger.debug(projectName, config, options)
     await publishMiniprogram({ projectName, config, publishVersion: options.publishVersion, desc: options.desc, robot: options.robot, policy: options.policy })
@@ -16,19 +18,20 @@ export const publishMiniprogramProjectAction = async (
 }
 
 /**
- * 显示所有配置
+ * 列表显示所有小程序项目
+ * @param options 
  */
-export const listConfigAction = async ({ raw }: listConfigActionOptions) => {
-  await getConfigList({ raw })
+export async function listConfigAction(options: listConfigActionOptions) {
+  await getConfigList(options)
 }
 
 /**
- * 
- * @param projectName 
- * @param version 
- * @returns 
+ * 新建/更新小程序项目
+ * 项目名称不存在就是新建,反之就是更新
+ * @param projectName 小程序项目名称 
+ * @param version 想要新建/更新的版本号
  */
-export const updateConfigAction = async (projectName: string, version: string) => {
+export async function updateConfigAction(projectName: string, version: string) {
   try {
     await updateVersion({ projectName, version })
   } catch (error: any) {
--- src/types.ts
+++ src/types.ts
@@ -11,10 +11,10 @@ export type ConfigurationOptions = {
 export type VersionUpdatePolicy = 'odd' | 'even' | 'standard'
 
 export type PublishMiniprogramActionOptions = {
-  publishVersion?: string,
-  desc?: string,
-  robot?: number,
-  policy?: VersionUpdatePolicy
+  publishVersion?: string, // 想要发布的版本号,默认会使用版本策略进行发布
+  desc?: string, // 发布时的描述,默认填入版本号
+  robot?: number, // 机器人编号,默认为1
+  policy?: VersionUpdatePolicy // 版本策略
 }
 
 export type PublishMiniprogramOptions = {
@@ -27,7 +27,7 @@ export type PublishMiniprogramOptions = {
 }
 
 export type listConfigActionOptions = {
-  raw?: boolean
+  raw?: boolean // 不采用列表输出, 输出原始格式
 }
 
 export type listConfigOptions = {
