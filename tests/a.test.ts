9c5081cb3c8ee9e34d77809708aa0f3d6dee1c92
kk++ <kkplusplus@163.com>
2023-05-25 11:22:37 +0800

    test: 提交测试用例

--- tests/a.test.ts
+++ tests/a.test.ts
@@ -1,11 +1,42 @@
-import { validateSemanticVersion } from "../src/util";
+import { validateSemanticVersion, nextSemanticVersion } from "../src/util";
+import { IDatabase } from "../src/database";
+import { T_Miniprogram_Project } from "../src/types";
 
-test('create project named P1', () => {
+jest.mock('../src/database.ts', () => {
+  class MockDatabase implements IDatabase {
+    connectionString: string
+
+    constructor(connectionString: string) {
+      this.connectionString = connectionString
+    }
+
+    T_MINIPROGRAM_PROJECT = {
+      list: async () => {
+        const t: T_Miniprogram_Project = {
+          p1: { version: '0.0.1' }
+        }
+        return t
+      },
+      update: (content: object) => { }
+    }
+  }
+  
+  return {
+    DATABASE: new MockDatabase("")
+  }
+})
+
+test('[validateSemanticVersion] standard', () => {
   const r = validateSemanticVersion('0.0.1', 'xxx')
   expect(r).toBeFalsy();
 });
 
-test('create project named P2', () => {
+test('[validateSemanticVersion] invalid input', () => {
   expect(() => validateSemanticVersion('aaa', 'xxx'))
     .toThrowError(new Error('xxx'));
+});
+
+test('[nextSemanticVersion] odd policy', async () => {
+  const v = await nextSemanticVersion('p1')
+  expect(v).toEqual('0.0.3')
 });
